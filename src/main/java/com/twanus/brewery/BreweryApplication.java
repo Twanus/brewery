a505e624c1115c197f819a0488ae4a1d23e25863
Vereecken <vereecken.antoon@hotmail.com>
2020-08-22 16:44:04 +0200

    ini commit

---
+++ src/main/java/com/twanus/brewery/BreweryApplication.java
@@ -0,0 +1,13 @@
+package com.twanus.brewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreweryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/twanus/brewery/BreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twanus.brewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
